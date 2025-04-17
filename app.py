7fbf673c78915daf1fa983b0df8d6d81feb8b3c4
abdul.moiz01 <abdul.moiz01@arbisoft.com>
2025-04-17 20:38:40 +0500

    Added history and made changes to get weather method

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify, request
-import requests
+import requests, json
 
 app = Flask(__name__)
 
 API_KEY = '3ac9190d2137d4d9534aaf185cc21e4d'
+
+weather_history = []
+MAX_HISTORY = 5
 
 @app.route('/')
 def home():
@@ -12,13 +15,42 @@ def home():
 @app.route('/weather')
 def get_weather():
     lat = request.args.get('lat', default=44.34)
+
     lon = request.args.get('lon', default=-10.99)
     
     url = f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_KEY}"
     
     response = requests.get(url)
-    data = response.json()
-    return jsonify(data)
+    data = json.loads(response.text)
+    
+    print('hwllo')
+    # Structured result using the JSON Response
+    result = {
+        "location": data.get("name"),
+        "temperature": {
+            "current": data["main"]["temp"],
+            "feels_like": data["main"]["feels_like"],
+            "min": data["main"]["temp_min"],
+            "max": data["main"]["temp_max"]
+        },
+        "pressure": data["main"]["pressure"],
+        "humidity": data["main"]["humidity"],
+        "weather": {
+            "main": data["weather"][0]["main"],
+            "description": data["weather"][0]["description"],
+            "icon": data["weather"][0]["icon"]
+        },
+        "wind": data.get("wind"),
+        "clouds": data.get("clouds"),
+        "rain": data.get("rain", {}),
+        "time": data.get("dt")
+    }
+    
+    weather_history.append(result)
+    if len(weather_history) > MAX_HISTORY:
+        weather_history.pop(0)
+
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
